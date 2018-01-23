bf281f9eb7fdd6a49b25f953aa07d57646c06ccd
Edward James <Edwards_fun@hotmail.com>
2018-01-23 23:01:40 +0000

    woooop

---
+++ Assets/ButtonManager.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class ButtonManager : MonoBehaviour {
+
+    private FileReader fileReader;
+    private InputField inputField;
+
+	// Use this for initialization
+	void Start () {
+        fileReader = FindObjectOfType<FileReader>();
+        inputField = GetComponentInChildren<InputField>();
+	}
+	
+    public void OnGenerateNamePressed()
+    {
+        string name = fileReader.GetName();
+        inputField.text = name;
+    }
+
+    public void OnRemoveNamePressed()
+    {
+        string name = inputField.text;
+        fileReader.RemoveName(name);
+    }
+}
---
+++ Assets/FileReader.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.IO;
+using System.Collections.Generic;
+
+public class FileReader : MonoBehaviour {
+
+    public TextAsset nameTextFile;
+
+    private List<string> names;
+
+    void Start()
+    {
+        ReadFromFile();
+    }
+    
+    void WriteToFile()
+    {
+        File.WriteAllText(Application.dataPath + "/" + nameTextFile.name + ".txt", string.Join("\n", names.ToArray()));
+        AssetDatabase.Refresh();        
+    }
+
+    void ReadFromFile()
+    {
+        names = new List<string>(nameTextFile.text.Split(new[] { '\n', '\r' }, System.StringSplitOptions.RemoveEmptyEntries));
+    }
+    
+    public string GetName()
+    {
+        return names[Random.Range(0, names.Count)];
+    }
+
+    public void RemoveName(string name)
+    {
+        int index = -1;
+        for (int i = 0; i < names.Count; i++)
+        {
+            if (names[i] == name)
+            {
+                index = i;
+            }
+        }
+
+        if (index >= 0)
+        {
+            names.RemoveAt(index);
+        }
+
+        WriteToFile();
+    }
+}
